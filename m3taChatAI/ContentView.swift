8deafaa264cb8368f3fd8ad7736064a8b4e10708
bekirkus <bekir.kus@hotmail.de>
2023-02-10 16:32:46 +0100

    Initial Commit

---
+++ m3taChatAI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  m3taChatAI
+//
+//  Created by Bekir Kus on 10.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m3taChatAI/m3taChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  m3taChatAIApp.swift
+//  m3taChatAI
+//
+//  Created by Bekir Kus on 10.02.23.
+//
+
+import SwiftUI
+
+@main
+struct m3taChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
